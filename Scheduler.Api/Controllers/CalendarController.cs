2230dc3a3da63dcf95dc805c3b179148b3df23b8
daniel.was <gatredev@gmail.com>
2026-02-10 23:40:20 +0100

    Minor naming changes

---
+++ Scheduler.Api/Controllers/CalendarController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Scheduler.Api.DTOs;
+using Scheduler.Api.Services;
+
+namespace Scheduler.Api.Controllers;
+
+[Route("/")]
+public class CalendarController : ControllerBase
+{
+    private readonly CalendarService _calendarService;
+
+    public CalendarController(CalendarService calendarService)
+    {
+        _calendarService = calendarService;
+    }
+
+    [HttpGet("query")]
+    public async Task<IActionResult> Query(AvailableSlotsRequest request) => Ok(await _calendarService.BuildCalendarAsync(request));
+}
